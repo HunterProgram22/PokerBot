43579cde7824982f059b6f7a00577d886aaa5d4a
HunterProgram22 <JustinKudela@gmail.com>
2018-05-18 11:15:54 -0400

    Added function to clean cards for capitalization and order and suited.

--- Poker.py
+++ Poker.py
@@ -4,6 +4,8 @@
 from Tournament import Tournament
 from Strategy import PositionStrategy
 
+REVERSE_HANDS = ["KA", "QK", "JQ", "TK", "TQ", "TJ", "KAs", "QKs", "JQs", "TKs", "TQs", "TJs"]
+NOREVERSE_HANDS = ["AK", "AKs", "KQ", "KQs", "QJ", "QJs", "JT", "JTs", "KT", "KTs", "QT", "QTs", "JT", "JTs"]
 
 players = int(input("Enter the number of players for the tournament: "))
 # name = input("Enter the tournament name: ")
@@ -13,15 +15,39 @@ players = int(input("Enter the number of players for the tournament: "))
 Tourn = Tournament(players)
 Main = PositionStrategy(Tourn)
 
+def clean_cards(cards):
+    cards = cards.upper()
+    if 'S' in cards:
+        cards = cards[:2] + cards[2:].lower()
+    print(cards)
+    if cards in REVERSE_HANDS:
+        if len(cards) == 2:
+            cards = cards[1] + cards[0]
+        else:
+            cards = cards[1] + cards[0] + cards[2]
+    elif cards in NOREVERSE_HANDS:
+        cards = cards
+    elif cards[0] < cards[1]:
+        if len(cards) == 2:
+            cards = cards[1] + cards[0]
+        else:
+            cards = cards[1] + cards[0] + cards[2]
+    print(cards)
+    return cards
+
 while True:
     cards = input("Enter your cards: ")
+    print(cards)
     if cards == "":
         break
-    try:
-        position = int(input("Enter your position: "))
-    except ValueError:
-        continue
-    Main.action(cards, position)
+    # Clean data to account for S capital for suited and entering
+    # data in reverse order.
+    cards = clean_cards(cards)
+    # try:
+    #     position = int(input("Enter your position: "))
+    # except ValueError:
+    #     continue
+    # Main.action(cards, position)
 
 
 
--- Strategy.py
+++ Strategy.py
@@ -1,8 +1,10 @@
+TIER_ONE = ["AA", "KK", "QQ", "JJ", "AK", "AKs"]
+TIER_TWO = ["TT", "99", "88", "77", "AQ", "AJ", "AQs", "AJs"]
+TIER_THREE = ["ATs", "KQs", "QJs", "JTs"]
+TIER_FOUR = ["66", "55", "44", "33", "22","A9s", "109s", "98s"]
+
+
 class PositionStrategy(object):
-    TIER_ONE = ["AA", "KK", "QQ", "JJ", "AK", "AKs"]
-    TIER_TWO = ["TT", "99", "88", "77", "AQ", "AJ", "AQs", "AJs"]
-    TIER_THREE = ["ATs", "KQs", "QJs", "JTs"]
-    TIER_FOUR = ["66", "55", "44", "33", "22","A9s", "109s", "98s"]
 
     def __init__(self, Tournament):
         self.tournament_position_dict = Tournament.create_position_dictionary()
@@ -21,53 +23,53 @@ class PositionStrategy(object):
             self.button_action()
 
     def small_blind_action(self):
-        if self.cards in PositionStrategy.TIER_ONE:
+        if self.cards in TIER_ONE:
             print("Raise limper or re-raise any standard bet.")
-        elif self.cards in PositionStrategy.TIER_TWO:
+        elif self.cards in TIER_TWO:
             print("Raise limper or call any standard bet.")
         else:
             print("Call in unraised pot, else fold.")
 
     def early_position_action(self):
-        if self.cards in PositionStrategy.TIER_ONE:
+        if self.cards in TIER_ONE:
             print("Raise limper or re-raise any standard bet.")
-        elif self.cards in PositionStrategy.TIER_TWO:
+        elif self.cards in TIER_TWO:
             print("Raise limper or call any standard bet.")
         else:
             print("Fold")
 
     def middle_position_action(self):
-        if self.cards in PositionStrategy.TIER_ONE:
+        if self.cards in TIER_ONE:
             print("Raise if first in, Else raise limper or reraise.")
-        elif self.cards in PositionStrategy.TIER_TWO:
+        elif self.cards in TIER_TWO:
             print("Raise if first in, Else call standard raise.")
-        elif self.cards in PositionStrategy.TIER_THREE:
+        elif self.cards in TIER_THREE:
             print("Raise if first in, Else call standard raise.")
         else:
             print("Fold")
 
     def late_position_action(self):
         print("If first in raise,")
-        if self.cards in PositionStrategy.TIER_ONE:
+        if self.cards in TIER_ONE:
             print("Else raise limper or reraise.")
-        elif self.cards in PositionStrategy.TIER_TWO:
+        elif self.cards in TIER_TWO:
             print("Else call standard raise.")
-        elif self.cards in PositionStrategy.TIER_THREE:
+        elif self.cards in TIER_THREE:
             print("Else call standard raise.")
-        elif self.cards in PositionStrategy.TIER_FOUR:
+        elif self.cards in TIER_FOUR:
             print("Else call standard raise.")
         else:
             print("Else fold.")
 
     def button_action(self):
         print("If first in raise,")
-        if self.cards in PositionStrategy.TIER_ONE:
+        if self.cards in TIER_ONE:
             print("Else raise limper or reraise.")
-        elif self.cards in PositionStrategy.TIER_TWO:
+        elif self.cards in TIER_TWO:
             print("Else call standard raise.")
-        elif self.cards in PositionStrategy.TIER_THREE:
+        elif self.cards in TIER_THREE:
             print("Else call standard raise.")
-        elif self.cards in PositionStrategy.TIER_FOUR:
+        elif self.cards in TIER_FOUR:
             print("Else call standard raise.")
         else:
             print("Else limp in or call standard raise with any two cards.")
