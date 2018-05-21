c9f17f71ae61b720a1de1e04e3819b99fdbc0db5
HunterProgram22 <HunterProgram22@github.com>
2018-05-21 16:06:21 -0400

    Began adding tournament and blind stucture.

--- Tournament.py
+++ Tournament.py
@@ -1,3 +1,6 @@
+import time
+
+
 class Tournament(object):
     """A tournament class to define various structures."""
 
@@ -34,3 +37,52 @@ class Tournament(object):
                     5: "Late Position",
                     6: "Button",
                     }
+
+
+class DoubleUpSngTurbo(Tournament):
+
+    def __init__(self, buyin, fee, players=6):
+        self.players = players
+        self.buyin = buyin
+        self.fee = fee
+        self.type = "Double-Up SNG"
+        self.start_time = time.time()
+        self.blinds = {
+            1: (0, 10, 20),
+            2: (0, 15, 30),
+            3: (0, 25, 50),
+            4: (0, 50, 100),
+            5: (0, 100, 200),
+            6: (0, 150, 300),
+            7: (30, 200, 400),
+            8: (45, 250, 500),
+            9: (50, 500, 1000)
+        }
+
+    def get_blind_level(self):
+        elapsed_time = round(time.time()-self.start_time, 3)
+        if elapsed_time < 300.0:
+            blind_level = self.blinds[1]
+        elif elapsed_time < 600.0:
+            blind_level = self.blinds[2]
+        print("Antes are {}, small blind is {}, and big blind is {}.".format(blind_level[0], blind_level[1], blind_level[2]))
+        return blind_level
+
+    def calculate_big_blinds_remaining(self, chip_stack):
+        big_blinds_remaining = chip_stack/self.get_blind_level()[2]
+        print(big_blinds_remaining)
+
+
+
+#Module Testing
+test = DoubleUpSngTurbo(20.00, 1.00, 6)
+time.sleep(5)
+#test.get_blind_level()
+test.calculate_big_blinds_remaining(1500)
+
+
+
+
+
+
+#
